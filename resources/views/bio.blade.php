dd4ac8b8027cde40004ba19b9fe0b9d4da047551
Zanyar <zaniarhd33@gmail.com>
2022-04-05 20:55:33 +0300

    initial commit

---
+++ resources/views/bio.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Biography</title>
+
+    <style>
+        html{
+            background-color: lightgray;
+        }
+        h1{
+            padding-top: 20px
+        }
+        img{
+            width: 100px;
+            height: 100px;
+            float: left;
+            margin: 0 20px 0 20px;
+        }
+    </style>
+</head>
+<body>
+    <div style="width: 80%; margin: 0 auto; background: grey; border-radius: 5px; min-height: 250px">
+        <h1 style="text-align: center">Biography</h1>
+        <img src="pics/test.jpg" alt="picture">
+        <p style="font-size: 20px; text-align:center;padding-left:10px;padding-right:10px; margin: 20px">
+            My name is Zanyar Omar, i was born and raised in Sulaimaniyah Kurdistan. I am twenty years old and
+            I am a student in Sulaimaniyah Polythechnic University, Technical College of Informatics. I major in Informtion Technlology with a minor in business.
+        </p>
+        <a href="bio.blade.php"></a>
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/zanyar.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <style>
+        html{
+            background-color: lightgrey;
+            text-align: center;
+        }
+        a{
+            color: black;
+        }
+        a:hover{
+            color: lightgrey;
+        }
+        h1{
+            padding-top: 20px
+        }
+        p{
+            padding-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div style="width: 80%; margin: 0 auto; background: grey; border-radius: 5px; min-height: 250px">
+        <h1>Hello there</h1>
+        <p style="font-size: 20px">
+            This is the home page!
+        </p>
+        <a href="bio.php">Check my profile</a>
+    </div>
+    
+</body>
+</html>
